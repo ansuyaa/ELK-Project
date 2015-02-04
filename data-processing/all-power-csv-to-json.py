9c79c5ee68083343240c7f146cb6f19725614837
Ansuya Ahluwalia <ansuya@Ansuyas-MacBook-Pro.local>
2015-02-04 15:47:25 -0800

    code for processing geo points based dataset

---
+++ data-processing/all-power-csv-to-json.py
@@ -0,0 +1,86 @@
+import csv
+import json
+from datetime import datetime
+
+def convert(csvFileName, fieldNames):
+	jsonFileName = csvFileName.split('.')[0] + '.json'
+	csvFile = open(csvFileName, 'r')
+	jsonFile = open(jsonFileName, 'w')
+
+	reader = csv.DictReader(csvFile, fieldNames)
+	for row in reader:
+		json.dump(row, jsonFile)
+		jsonFile.write('\n')
+
+def convert_nested_flattened_2(csvFileName, dates):
+	jsonFileName = csvFileName.split('.')[0] + '-flat.json'
+	jsonFile = open(jsonFileName, 'w')
+	n_dates = len(dates)
+
+	with open(csvFileName) as f:
+		lis=[line.split() for line in f]
+
+		for x in lis:
+			l = x[0].split(",")
+			i_row = 3
+			j_row = n_dates + 3
+			while i_row <= n_dates :
+				row = dict()
+				row['ID'] = l[0]
+				row['long'] = l[1]
+				row['lat'] = l[2]
+				row["date"] = dates[i_row - 3]
+				row["usage_amount"] = l[i_row]
+				row["users"] = l[j_row]
+				json.dump(row, jsonFile)
+				jsonFile.write('\n')
+				i_row = i_row + 1
+				j_row = j_row + 1
+
+def convert_nested_flattened_3(csvFileName, dates):
+	jsonFileName = csvFileName.split('.')[0] + '-flat.json'
+	jsonFile = open(jsonFileName, 'w')
+	n_dates = len(dates)
+
+	with open(csvFileName) as f:
+		lis=[line.split() for line in f]
+
+		for x in lis:
+			l = x[0].split(",")
+			i_row = 3
+			j_row = n_dates + 3
+			k_row = n_dates * 2 + 3
+			while i_row <= n_dates :
+				row = dict()
+				row['ID'] = l[0]
+				row['long'] = l[1]
+				row['lat'] = l[2]
+				row["date"] = dates[i_row - 3]
+				row["min_temp"] = l[i_row]
+				row["max_temp"] = l[j_row]
+				row["mean_temp"] = l[k_row]
+				json.dump(row, jsonFile)
+				jsonFile.write('\n')
+				i_row = i_row + 1
+				j_row = j_row + 1
+				k_row = k_row + 1
+
+
+fieldNames = ("ID","sqmi","long","lat","population","age_0_5","age_6_9","age_10_14","age_15_17","age_18_24",
+	"age_25_34","age_35_44","age_45_54","age_55_64","age_64_74","age_75_84","age_85_200","average_household_income","average_family_income"
+	,"per_capita_income","housing_unit_total","housing_unit_occupied","housing_unit_vacant","median_year_built","average_rent","Elevation")
+csvFileName = 'CCSC_dataset_and_demo_analysis/full-data/Consumption_Blockgroup_Sharable_Detail_with_Temperatures/all_census.csv'
+convert(csvFileName, fieldNames)
+
+
+#convert string to JSON datetime format
+#str(datetime(2006,1,1)).replace(' ','T')
+#'2006-01-01T00:00:00'
+dates = [ str(datetime(year,month,1)).replace(' 00:00:00','') for year in [2006,2007,2008, 2009, 2010, 2011] for month in [1,2,3,4,5,6,7,8,9,10,11,12] ]
+csvFileName = 'CCSC_dataset_and_demo_analysis/full-data/Consumption_Blockgroup_Sharable_Detail_with_Temperatures/all_temp.csv'
+convert_nested_flattened_3(csvFileName, dates)
+
+csvFileName = 'CCSC_dataset_and_demo_analysis/full-data/Consumption_Blockgroup_Sharable_Detail_with_Temperatures/all_usage.csv'
+convert_nested_flattened_2(csvFileName, dates)
+
+
--- data-processing/power-processing.R
+++ data-processing/power-processing.R
@@ -3,4 +3,19 @@ power = read.table("All_CCSC_Data_in_one_Table.csv", header=TRUE, na.strings = "
 head(power)
 colnames(power)
 
-#to load file to elasticsearch, use logstash
+building_type_land_per = read.table("Building_Type_Land_Percentage.csv", header=TRUE, na.strings = ".", sep=",")
+census_noised = read.table("Census_Noised.csv", header=TRUE, na.strings = ".", sep=",")
+consumpiton = read.table("All_CCSC_Data_in_one_Table.csv", header=TRUE, na.strings = ".", sep=",")
+
+all_power = read.table("All_Blockgroup_Data_averaged_by_Month.csv", header=TRUE, na.strings = ".", sep=",")
+all_consumption = read.table("Consumption_Blockgroup_Sharable_Detail_with_Temperatures.csv", header=TRUE, na.strings = ".", sep=",")
+
+all_census = all_consumption[,c("BlockgroupID","sqmi","longitude","latitude","population","age_0_5","age_6_9","age_10_14","age_15_17","age_18_24",
+	"age_25_34","age_35_44","age_45_54","age_55_64","age_64_74","age_75_84","age_85_200","average_household_income","average_family_income"
+	,"per_capita_income","housing_unit_total","housing_unit_occupied","housing_unit_vacant","median_year_built","average_rent","Elevation")]
+all_temp = all_consumption[,c(1,5,6,c(29:244))]
+all_usage = all_consumption[,c(1,5,6,c(245:388))]
+
+write.table(all_census, "all_census.csv", row.names = FALSE, sep=",", col.names = FALSE)
+write.table(all_temp, "all_temp.csv", row.names = FALSE, sep=",", col.names = FALSE)
+write.table(all_usage, "all_usage.csv", row.names = FALSE, sep=",", col.names = FALSE)
